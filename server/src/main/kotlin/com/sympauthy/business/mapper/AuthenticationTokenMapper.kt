4e259f27fa580dc50ce7ddca17dd77489838d9d4
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2026-02-27 17:42:37 +0100

    Add validation for AuthenticationToken coherence in AuthenticationTokenMapper
    Introduce validation logic in `AuthenticationTokenMapper` to ensure data consistency based on the `grantType` field. Added error messages for different validation scenarios in `error_messages.properties`.

--- server/src/main/kotlin/com/sympauthy/business/mapper/AuthenticationTokenMapper.kt
+++ server/src/main/kotlin/com/sympauthy/business/mapper/AuthenticationTokenMapper.kt
@@ -1,14 +1,52 @@
 package com.sympauthy.business.mapper
 
+import com.sympauthy.business.exception.internalBusinessExceptionOf
 import com.sympauthy.business.mapper.config.ToBusinessMapperConfig
 import com.sympauthy.business.model.oauth2.AuthenticationToken
 import com.sympauthy.data.model.AuthenticationTokenEntity
+import org.mapstruct.AfterMapping
 import org.mapstruct.Mapper
+import org.mapstruct.MappingTarget
 
 @Mapper(
     config = ToBusinessMapperConfig::class
 )
-interface AuthenticationTokenMapper {
+abstract class AuthenticationTokenMapper {
 
-    fun toToken(entity: AuthenticationTokenEntity): AuthenticationToken
+    abstract fun toToken(entity: AuthenticationTokenEntity): AuthenticationToken
+
+    @AfterMapping
+    protected fun validateTokenCoherence(@MappingTarget token: AuthenticationToken) {
+        when (token.grantType) {
+            "client_credentials" -> {
+                if (token.userId != null) {
+                    throw internalBusinessExceptionOf(
+                        "mapper.authentication_token.client_credentials.invalid_user_id",
+                        "grantType" to token.grantType
+                    )
+                }
+                if (token.authorizeAttemptId != null) {
+                    throw internalBusinessExceptionOf(
+                        "mapper.authentication_token.client_credentials.invalid_authorize_attempt_id",
+                        "grantType" to token.grantType
+                    )
+                }
+            }
+
+            else -> {
+                if (token.userId == null) {
+                    throw internalBusinessExceptionOf(
+                        "mapper.authentication_token.missing_user_id",
+                        "grantType" to token.grantType
+                    )
+                }
+                if (token.authorizeAttemptId == null) {
+                    throw internalBusinessExceptionOf(
+                        "mapper.authentication_token.missing_authorize_attempt_id",
+                        "grantType" to token.grantType
+                    )
+                }
+            }
+        }
+    }
 }
